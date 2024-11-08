48859128c62c28d8d09ebeffa917280ee6b558db
zeyuyuan <yzy@pcomg.com>
2024-11-08 09:35:12 +0800

    first commit

---
+++ components/Counter/Counter.tsx
@@ -0,0 +1,34 @@
+"use client";
+import React, { useState } from "react";
+import styles from "./styles.module.css";
+
+interface CounterProps {
+  initialCount?: number;
+}
+
+function Counter({ initialCount = 99 }: CounterProps) {
+  const [count, setCount] = useState(initialCount);
+
+  const increment = () => {
+    setCount((prevCount) => prevCount + 1);
+  };
+
+  const decrement = () => {
+    setCount((prevCount) => prevCount - 1);
+  };
+
+  return (
+    <div className={styles.counter}>
+      <div>test counter</div>
+      <button className={styles.btn} onClick={decrement}>
+        -
+      </button>
+      <span className={styles.count}>{count}</span>
+      <button className={styles.btn} onClick={increment}>
+        +
+      </button>
+    </div>
+  );
+}
+
+export default Counter;
